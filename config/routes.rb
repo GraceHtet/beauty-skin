f51c1df77ef1b234b947adf83c22ebd381e8661e
Thiri <thirihtet246@gmail.com>
2023-10-24 19:58:29 +0630

    Create Staff model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :staffs, path: 'staffs', controller: {
+    sessions: 'staffs/sessions'
+  }
+
   devise_for :clients, path: 'clients', controller: {
     sessions: 'clients/sessions'
   }
